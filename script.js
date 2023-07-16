90424fefd0aa5b4d1da39e0b07930972c5336ce4
Serene Aryal <serene.aryal24@gmail.com>
2023-07-16 16:44:48 +0545

    About me section done.

---
+++ script.js
@@ -0,0 +1,15 @@
+const tabLinks = document.getElementsByClassName("tab-links")
+const tabContents = document.getElementsByClassName("tab-content")
+
+function openTab(tab){
+    console.log("skdhs")
+    for(tabLink of tabLinks){
+        tabLink.classList.remove("active");
+    }
+
+    for(tabContent of tabContents){
+        tabContent.classList.remove("active");
+    }
+    document.getElementById(tab).classList.add('active');
+    event.currentTarget.classList.add('active');
+}
